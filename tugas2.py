f51c2851f5211acf3888ba0ae8eeb8e65a1f69eb
sintanabilasoraya <sintanabilasoraya17@gmail.com>
2025-04-22 12:38:20 +0800

    Delete tugas2.py

--- tugas2.py
+++
@@ -1,6 +0,0 @@
-# Input: jumlah deret
-jumlah_deret = int(input("Masukkan jumlah deret: "))
-
-# Menghitung bilangan genap
-bilangan_genap = [i for i in range(2, jumlah_deret + 1) if i % 2 == 0]
-print("Output:", ', '.join(map(str, bilangan_genap)))
